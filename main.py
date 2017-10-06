17f56cf22ac791520c37c1ea239addb00628f595
bb20basketball <bb20basketball@gmail.com>
2017-10-05 19:10:06 -0500

    simple test to start

---
+++ main.py
@@ -0,0 +1,28 @@
+import json
+import urllib
+
+class getTemperature(object):
+
+    def __init__(self):
+
+        self.update()
+
+    def update(self):
+
+        self.getData = urllib.urlopen('http://api.openweathermap.org/data/2.5/weather?q=minneapolis,us&APPID=e43c3669282ae317bc20c475813df4b7')
+        self.dataToJSON = json.load(self.something)
+        print dataToJSON['main']['temp']
+
+class main(object):
+
+    def __init__(self):
+        # GPIO stuff
+        pass
+
+    def checkTemp(self):
+
+        pass
+
+if __name__ == "__main__":
+    # just testing for now
+    getTemperature()
