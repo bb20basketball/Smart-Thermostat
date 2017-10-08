b1d47633d5ac6f3220f641e5a997258482347e19
bb20basketball <bb20basketball@gmail.com>
2017-10-07 23:17:00 -0500

    made main for functional

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import time
+import threading
 
 from components.getTemperature import GetTemperature
-from components.servoControl import ServoControl
+#from components.servoControl import ServoControl
 
 class main(object):
 
@@ -11,22 +12,54 @@ class main(object):
         # current thermostat setpoint (in F degrees)
         self.currentSetpoint = 70
 
+        self.comfortableTemp = 73
+
+        self.schedule = {"Sunday": (12,15), "Monday": (8,17), "Tuesday": (8,17),
+         "Wednesday":(8, 17), "Thursday":(8,17), "Friday":(8,17), "Saturday":(11, 22)}
+
         # temperature API class
         self.getTemp = GetTemperature()
-        self.servoControl = ServoControl()
+        #self.servoControl = ServoControl()
         print (self.getTemp.update())
+        self.checkTemp()
 
     def checkTemp(self):
         """
         Checks if thermostat should adjust or not
         """
         currentTemp = self.getTemp.update()
+        currentHour =  int(time.strftime("%H"))
+        startHour = self.schedule[time.strftime("%A")][0]
+        endHour = self.schedule[time.strftime("%A")][1]
+
+        if currentHour > startHour and currentHour < endHour:
+            print 'uyp'
+        elif currentHour == endHour:
+            print 'yo'
+            self.changeTemp(self.comfortableTemp)
+        elif currentHour == startHour:
+            print 'y'
+            self.changeAlgo(currentTemp)
+
+        #threading.Timer(60.0, self.checkTemp).start()
+
+    def changeAlgo(self, currentTemp):
+        setTemp = currentTemp
+
+        if currentTemp < 69:
+            setTemp = 69
+        elif currentTemp > 78:
+            setTemp = 78
+
+        self.changeTemp(setTemp)
 
     def changeTemp(self, setpoint):
         """
         Uses the servo to set the temperature on the thermostat
         """
-        pass
+        if setpoint != self.currentSetpoint:
+            self.currentSetpoint = setpoint
+            print "doing servo stuff"
 
 if __name__ == "__main__":
     main()
